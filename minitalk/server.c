b123202266f7efd13fad58dabe432e321d48db7d
Ariclenes Pereira <arpereir@c2r6s12.42lisboa.com>
2025-09-23 19:25:35 +0100

    Refresh Minitalk

--- minitalk/server.c
+++ minitalk/server.c
@@ -2,7 +2,6 @@
 #include "libft/libft.h"
 #include <signal.h>
 
-/*
 void    handler(int sig)
 {
     static int     bit;
@@ -22,30 +21,6 @@ void    handler(int sig)
         c = 0;
     }
 }
-*/
-void handler(int sig)
-{
-    static int bit_count = 0;
-    static unsigned char c = 0;
-
-    if (sig == SIGUSR1)
-        c |= (1 << (7 - bit_count)); // coloca 1 no bit correto
-    // se for SIGUSR2 → bit 0, não precisa fazer nada
-
-    bit_count++;
-
-    if (bit_count == 8)
-    {
-        if (c == 0)
-            write(1, "\n", 1);
-        else
-            write(1, &c, 1);
-
-        bit_count = 0;
-        c = 0;
-    }
-}
-
 
 int main(int argc, char **argv)
 {
