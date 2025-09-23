a94bc8cd37bc4d8b6fa003030e213dd344036cc8
Ariclenes Pereira <arpereir@c2r6s12.42lisboa.com>
2025-09-23 19:23:50 +0100

    Refresh Minitalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -2,15 +2,36 @@
 #include <signal.h>
 #include "libft/libft.h"
 
+void    send_bits(int pid, char c)
+{
+    int bit;
+    int i;
+
+    i = 7;
+     while (i >= 0)
+    {
+        bit = (c >> i) & 1;
+        if (bit == 1)
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        usleep(1500);
+        i--;
+    }
+}
+
 int main(int argc, char **argv)
 {
+    int i;
     int pid;
 
     pid = ft_atoi(argv[1]);
-
-    if (kill(pid, SIGUSR1) == -1)
-        perror("erro ao enviar sinal\n");
-    else
-        ft_printf("Enviei sinal para o pid: %i\n", pid);
+    i = 0;
+    while (argv[2][i] != '\0')
+    {
+        send_bits(pid, argv[2][i]);
+        i++;
+    }
+    send_bits(pid, '\0');
     return (0);
 }
