4866b70ae0d39b7eb433a710b3f78d6d2d9020a6
Ariclenes Pereira <arpereir@c2r3s8.42lisboa.com>
2025-09-30 17:15:57 +0100

    Refresh Minitalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,48 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arpereir <arpereir@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 17:00:43 by arpereir          #+#    #+#             */
+/*   Updated: 2025/09/30 17:06:10 by arpereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <signal.h>
 #include "libft/libft.h"
 
-int signal_flag = 0;
+int	g_signal_flag = 0;
 
-void    signal_handler(int sig)
+void	signal_handler(int sig)
 {
-    (void)sig;
-    signal_flag = 1;
+	(void)sig;
+	g_signal_flag = 1;
 }
 
-void    send_bits(int pid, char c)
+void	send_bits(int pid, char c)
 {
-    int bit;
-    int i;
+	int	bit;
+	int	i;
 
-    i = 7;
-     while (i >= 0)
-    {
-        bit = (c >> i) & 1;
-        if (bit == 1)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        while (!signal_flag)
-            pause();
-        i--;
-    }
+	i = 7;
+	while (i >= 0)
+	{
+		bit = (c >> i) & 1;
+		g_signal_flag = 0;
+		if (bit == 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		while (!g_signal_flag)
+			pause();
+		i--;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int i;
-    int pid;
+	int	i;
+	int	pid;
 
-    signal(SIGUSR1, signal_handler);
-
-    pid = ft_atoi(argv[1]);
-    i = 0;
-    while (argv[2][i] != '\0')
-    {
-        send_bits(pid, argv[2][i]);
-        i++;
-    }
-    send_bits(pid, '\0');
-    return (0);
+	signal(SIGUSR1, signal_handler);
+	pid = ft_atoi(argv[1]);
+	i = 0;
+	while (argv[2][i] != '\0')
+	{
+		send_bits(pid, argv[2][i]);
+		i++;
+	}
+	send_bits(pid, '\0');
+	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,42 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arpereir <arpereir@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/30 16:56:22 by arpereir          #+#    #+#             */
+/*   Updated: 2025/09/30 17:00:37 by arpereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define _GNU_SOURCE
 #include <unistd.h>
 #include "libft/libft.h"
 #include <signal.h>
 
-void    handler(int sig, siginfo_t *info, void *context)
+void	handler(int sig, siginfo_t *info, void *context)
 {
-    static int     bit;
-    static unsigned char c;
+	static int				bit;
+	static unsigned char	c;
 
-    c <<= 1;
-    if (sig == SIGUSR1)  
-        c |= 1;
-    bit++;
-    if (bit == 8)
-    {
-        if (c != '\0')
-            write (1, &c, 1);
-        else
-            write(1, "\n", 1);
-        bit = 0;
-        c = 0;
-    }
-    kill(info->si_pid, SIGUSR1);
+	(void)context;
+	c <<= 1;
+	if (sig == SIGUSR1)
+		c |= 1;
+	bit++;
+	if (bit == 8)
+	{
+		if (c != '\0')
+			write (1, &c, 1);
+		else
+			write(1, "\n", 1);
+		bit = 0;
+		c = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    struct sigaction sa;
+	struct sigaction	sa;
 
-    sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = handler;
-    sa.sa_flags = SA_SIGINFO;
-
-    ft_printf("PID do Servidor: %i\n", getpid());
-
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-    
-    while (1)
-        pause();
-    return (0);
+	sa.sa_sigaction = handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	ft_printf("PID do Servidor: %i\n", getpid());
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
 }
